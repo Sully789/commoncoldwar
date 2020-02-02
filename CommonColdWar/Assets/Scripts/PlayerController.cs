ee6627bb0b6b0dd4553c34e73d613a0bfd8d37d4
Sully789 <43644133+Sully789@users.noreply.github.com>
2020-02-02 21:20:04 +0000

    Basic Set Up
    v:0.01
    Began basic set up of game.
    -Adjusted Build Settings to target.
    -Imported Assets
    -Added Background
    -Added Player Sprite
    -Added JoyStick
    -Added Basic Touch Controls
    
    Next Steps:
    -Fix Controls
    -Add Shooting
    -Add Enemies

---
+++ CommonColdWar/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float horizontalMove;
+    public float verticalMove;
+
+    public Joystick joystick;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = joystick.Horizontal * moveSpeed;
+        verticalMove = joystick.Vertical * moveSpeed;
+
+        if (joystick.Horizontal >= .2f)
+        {
+            horizontalMove = moveSpeed;
+        }
+        else if(joystick.Horizontal <= -.2f)
+        {
+            horizontalMove = -moveSpeed;
+        }
+        else
+        {
+            horizontalMove = 0;
+        }
+
+        if (joystick.Vertical >= .2f)
+        {
+            verticalMove = moveSpeed;
+        }
+        else if (joystick.Vertical <= -.2f)
+        {
+            verticalMove = -moveSpeed;
+        }
+        else
+        {
+            verticalMove = 0;
+        }
+
+        // for(int i = 0; i < Input.touchCount; i++)
+        // {
+        //     Vector3 touchPositon = Camera.main.ScreenToViewportPoint(Input.touches[i].position);
+        //    Debug.DrawLine(Vector3.zero, touchPositon, Color.blue);
+        //  }
+    }
+}
